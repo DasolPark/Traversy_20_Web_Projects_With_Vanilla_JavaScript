a46845f374f0d6945901bedf3c6cd0fc3eeeedd6
Dasol Park <dasol.park615@gmail.com>
2020-04-23 15:51:20 +0900

    [5. DOM Array Methods] Double Money - map()

--- DOM-Array-Methods/script.js
+++ DOM-Array-Methods/script.js
@@ -27,6 +27,16 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+// Double everyones money
+function doubleMoney() {
+  data = data.map(user => {
+    return { ...user, money: user.money * 2 };
+  });
+
+  updateDOM();
+}
+
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -53,3 +63,4 @@ function formatMoney(number) {
 }
 
 addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
