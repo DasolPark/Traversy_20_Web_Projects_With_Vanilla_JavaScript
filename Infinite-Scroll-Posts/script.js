232d9c7e3293f2b75e3bf10bbeb8bee59d73e400
Dasol Park <dasol.park615@gmail.com>
2020-06-08 14:18:32 +0900

    [11. Infinite Scroll Posts] Filter Fetched Posts

--- Infinite-Scroll-Posts/script.js
+++ Infinite-Scroll-Posts/script.js
@@ -47,19 +47,32 @@ function showLoading() {
   }, 1000);
 }
 
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
 // Show initial posts
 showPosts();
 
 window.addEventListener('scroll', () => {
-  console.dir(document.documentElement);
-  console.log(document.documentElement.scrollTop);
-  console.log(document.documentElement.scrollHeight);
-  console.log(document.documentElement.clientHeight);
-
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
   if (scrollTop + clientHeight >= scrollHeight - 5) {
-    // console.log(123);
     showLoading();
   }
-})
+});
+
+filter.addEventListener('input', filterPosts)
