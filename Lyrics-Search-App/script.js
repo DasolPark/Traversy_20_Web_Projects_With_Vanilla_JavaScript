86704e6794b1056c870076c3f080cae4f78cb2b0
Dasol Park <dasol.park615@gmail.com>
2020-06-29 16:53:42 +0900

    [Lyrics Search App] Update DOM(Method 1&2)

--- Lyrics-Search-App/script.js
+++ Lyrics-Search-App/script.js
@@ -7,27 +7,50 @@ const apiURL = 'https://api.lyrics.ovh';
 
 // Search by song or artist
 async function searchSongs(term) {
-  // fetch(`${apiURL}/suggest/${term}`)
-  //   .then(res => res.json())
-  //   .then(data => console.log(data));
-
   const res = await fetch(`${apiURL}/suggest/${term}`);
   const data = await res.json();
-
-  console.log(data);
 
   showData(data);
 }
 
 // Show song and artist in DOM
-function showData(data) { };
+function showData(data) {
+  // 방법 1
+  // let output = '';
+
+  // data.data.forEach(song => {
+  //   output += `
+  //     <li>
+  //       <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //       <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+  //     </li>
+  //   `;
+  // });
+
+  // result.innerHTML = `
+  //   <ul class="songs">
+  //     ${output}
+  //   </ul>
+  // `;
+
+  // 방법 2
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data.map(song => `
+        <li>
+          <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+          <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>
+      `).join('')}
+    </ul>
+  `;
+};
 
 // Event listeners
 form.addEventListener('submit', e => {
   e.preventDefault();
 
   const searchTerm = search.value.trim();
-  // console.log(searchTerm);
 
   if (!searchTerm) {
     alert('Please type in a search term');
